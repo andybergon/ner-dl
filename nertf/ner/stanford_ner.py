a746fb608326662fbc0c170230ab7d34233af192
andybergon <andybergon@gmail.com>
2016-10-19 15:08:14 +0200

    minor fix ner stanford

--- nertf/ner/stanford_ner.py
+++ nertf/ner/stanford_ner.py
@@ -3,9 +3,9 @@ from nltk.tag.stanford import StanfordNERTagger
 import settings
 
 
-class StanfordNERModel():
+class StanfordNERModel:
     def __init__(self):
         self.classifier = StanfordNERTagger(settings.STANFORD_NER_CLASSIFIER, settings.STANFORD_NER_JAR)
 
-    def tag(self, sentence):
+    def predict_sentence(self, sentence):
         self.classifier.tag(sentence.split())  # TODO: use tokenizer
