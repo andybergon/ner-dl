c239a7d88494396df2d53446646b0c8a819505c6
andybergon <andybergon@gmail.com>
2016-11-14 10:11:49 +0100

    figer model and socket

---
+++ nerdl/ner/models/figer/figer_client_socket.py
@@ -0,0 +1,51 @@
+import socket
+
+
+class FigerSocket:
+    def __init__(self):
+        self.figer_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.figer_socket.connect(("localhost", 5000))
+
+    def request_prediction(self, sentence, recv_byte=2048, debug=0):
+        sentence += '\n'  # needed for TCP protocol
+
+        self.figer_socket.send(sentence)
+        if debug != 0:
+            print('Sent: {}'.format(sentence))
+
+        response = self.figer_socket.recv(recv_byte)
+        if debug != 0:
+            print('Received: {}'.format(response))
+
+        response = response[2:]  # trims strange char at start
+        formatted_prediction = format_prediction(response)
+
+        return formatted_prediction
+
+    def close(self):
+        self.figer_socket.close()
+
+
+def format_prediction(prediction):
+    formatted_prediction = []
+    word_tags_scores = prediction.split('\n')
+
+    for pair in word_tags_scores:
+        word, tags_scores = pair.split('\t')
+        tags_scores_formatted = []
+
+        for tag_score in tags_scores.split(','):
+            if tag_score == '':
+                tag = 'O'
+                score = 1
+            else:
+                tag, score = tag_score.split('@')
+                tag = tag.replace('/', '', 1)
+                tag = tag.replace('/', '.')
+                tag = tag.upper()
+
+            tags_scores_formatted.append((tag,score))
+
+        formatted_prediction.append((word, tags_scores_formatted))
+
+    return formatted_prediction
---
+++ nerdl/ner/models/figer/figer_model.py
@@ -0,0 +1,63 @@
+from nerdl.ner.models.figer.figer_client_socket import FigerSocket
+from nerdl.ner.models.model import Model
+from nerdl.ner.utils import tokenizer
+
+
+class FigerNERModel(Model):
+    def __init__(self, nb_classes=4):
+        self.socket = FigerSocket()
+
+    def predict_tokenized_sentence(self, tokenized_sentence):
+        untokenized_sentence = tokenizer.untokenize_word(tokenized_sentence)
+        prediction = self.socket.request_prediction(untokenized_sentence)
+
+        if len(prediction) != len(tokenized_sentence):
+            print('Prediction and tokenized sentence have different lengths!')
+
+        prediction = cutoff_top(prediction)
+
+        return prediction
+
+    def predict_sentence(self, sentence):
+        tokenized_sentence = tokenizer.tokenize_word(sentence)
+
+        return self.predict_tokenized_sentence(tokenized_sentence)
+
+
+def cutoff_top(prediction):
+    prediction = normalize_scores(prediction)
+    word_types = []
+
+    # TODO: top x
+    for word_typescore in prediction:
+        types = []
+        for type_score in word_typescore[1]:
+            types.append(type_score[0])
+        word_types.append((word_typescore[0], types))
+
+    return word_types
+
+
+def normalize_scores(prediction):
+    normalized_prediction = []
+
+    for pred_tuple in prediction:
+        word, types_scores = pred_tuple
+        sum_score = 0
+        new_types_scores = []
+        for type, score in types_scores:
+            sum_score += float(score)
+        for type, score in types_scores:
+            new_type_score = (type, float(score) / sum_score)
+            new_types_scores.append(new_type_score)
+    normalized_prediction.append((word, new_types_scores))
+
+    # for pred_tuple in prediction:
+    #     word, types_scores = pred_tuple
+    #     new_types_scores = []
+    #     for type, score in types_scores:
+    #         new_score = float(score) / sum_score
+    #         new_types_scores.append((type, new_score))
+    #     normalized_prediction.append(word, new_types_scores)
+
+    return normalized_prediction
