de1f95fa8a5295d2f33be972ef17e2eab8ee3ac8
andybergon <andybergon@gmail.com>
2016-11-19 20:12:49 +0100

    improve performance of figer tagger

--- nerdl/dataset/taggers/figer_tagger.py
+++ nerdl/dataset/taggers/figer_tagger.py
@@ -5,6 +5,17 @@ from tagger import Tagger
 class FigerTagger(Tagger):
     def __init__(self):
         self.mapping_file = path_settings.FIGER_ENTITY_MAPPING
+        self.freebase2figer_map = {}  # e.g. AMUSEMENT_PARKS.PARK -> PARK
+
+        self.initialize_map()
+
+    def initialize_map(self):
+        with open(self.mapping_file, 'rt') as mapping_f:
+            for line in mapping_f:
+                freebase_type, figer_type = line.rstrip('\n').split('\t')
+                freebase_type = freebase_type.replace('/', '', 1).replace('/', '.').upper()
+                figer_type = figer_type.replace('/', '', 1).replace('/', '.').upper()
+                self.freebase2figer_map[freebase_type] = figer_type
 
     def tag(self, entity_types):
         figer_types = self.convert_freebase_to_figer_types(entity_types)
@@ -12,20 +23,17 @@ class FigerTagger(Tagger):
         return figer_types
 
     def convert_freebase_to_figer_types(self, freebase_types):
+        """
+
+        :param freebase_types: e.g. [AWARD.AWARD, MUSIC.ALBUM]
+        :return: e.g. [AWARD, MUSIC]
+        """
         figer_types = []
 
-        # TODO: optimize, open file at construction and method to close
-        with open(self.mapping_file, 'rt') as mapping_f:
-            for fb_type in freebase_types:
-                mapping_f.seek(0)
-                for line in mapping_f:
-                    freebase_type, figer_type = line.rstrip('\n').split('\t')
-                    freebase_type = freebase_type.replace('/', '', 1).replace('/', '.')
-
-                    if fb_type == freebase_type:
-                        figer_type = figer_type.replace('/', '', 1).replace('/', '.')
-                        figer_types.append(figer_type.upper())
-                        break
+        for freebase_type in freebase_types:
+            if freebase_type in self.freebase2figer_map:
+                figer_type = self.freebase2figer_map[freebase_type]
+                figer_types.append(figer_type)
 
         figer_types = sorted(set(figer_types))
 
