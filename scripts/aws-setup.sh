85f3bb0924169cc80b9295505b636d21c84618ef
andybergon <andybergon@gmail.com>
2016-11-25 12:59:31 +0100

    renaming aws scripts

--- scripts/aws-setup.sh
+++ scripts/aws-setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # set -x;
 
+sudo yum update -y
 sudo yum install git
 git clone https://github.com/andybergon/ner-dl.git
 
 ner-dl/scripts/convert-settings-examples.sh
+ner-dl/scripts/aws-s3-download.sh
 
 wget https://repo.continuum.io/archive/Anaconda2-4.2.0-Linux-x86_64.sh
 bash Anaconda2-4.2.0-Linux-x86_64.sh -b -p $HOME/anaconda2
@@ -16,7 +18,7 @@ conda upgrade --all
 conda install nltk
 ner-dl/scripts/nltk_download.py
 
-conda install scipy numpy wheel pandas matplotlib scikit-learn gensim -y # All requested packages already installed.
+conda install scipy numpy wheel pandas matplotlib scikit-learn gensim pickle -y # All requested packages already installed.
 
 conda install -c conda-forge tensorflow -y # 0.10.0 # 0.11.0rc2
 conda install -c conda-forge theano -y # 0.8.2
