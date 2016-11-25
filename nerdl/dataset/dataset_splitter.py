b664af7df135fe38278515f232d9aff8081cb4e8
andybergon <andybergon@gmail.com>
2016-11-25 19:51:10 +0100

    dataset splitter print

--- nerdl/dataset/dataset_splitter.py
+++ nerdl/dataset/dataset_splitter.py
@@ -3,21 +3,18 @@ import random
 random.seed(0)
 
 
-def split_dataset(traintest_fp, train_fp, test_fp, test_perc=0.2, sentence_nb=0, print_every=100000):
+def split_dataset(dataset_fp, train_fp, test_fp, test_perc=0.2, sentence_nb=0, print_every=100000):
     current_sentence = 0
     tagged_words = []
 
-    with open(traintest_fp) as in_f, \
-            open(train_fp, 'w') as training_f, \
-            open(test_fp, 'w') as test_f:
-
+    with open(dataset_fp) as in_f, open(train_fp, 'w') as train_f, open(test_fp, 'w') as test_f:
         for line in in_f:
             if line != '\n':
                 word, tag = line.rstrip().split('\t')
                 tagged_words.append((word, tag))
             else:
                 if random.uniform(0, 1) > test_perc:
-                    to_write_f = training_f
+                    to_write_f = train_f
                 else:
                     to_write_f = test_f
 
@@ -35,4 +32,7 @@ def split_dataset(traintest_fp, train_fp, test_fp, test_perc=0.2, sentence_nb=0,
                     print('Sentence #{}/{}'.format(current_sentence, sentence_nb))
 
                 if current_sentence == sentence_nb:
+                    print('Splitted #{}/{} sentences.'.format(current_sentence, sentence_nb))
                     return
+
+    print('Splitted ALL #{}/{} sentences.'.format(current_sentence, sentence_nb))
