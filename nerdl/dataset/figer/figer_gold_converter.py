fa587975fa8336862b7f8b3efcd7e4bcdcbce409
andybergon <andybergon@gmail.com>
2016-11-25 19:59:38 +0100

    figer gold converter

---
+++ nerdl/dataset/figer/figer_gold_converter.py
@@ -0,0 +1,37 @@
+from settings import path_settings
+
+figer_gold_fp = path_settings.FIGER_GOLD_FILE
+
+figer_gold_bio_fp = path_settings.FIGER_GOLD_BIO_FILE
+figer_gold_not_bio_fp = path_settings.FIGER_GOLD_NOT_BIO_FILE
+
+
+def convert_figer_gold_to_bio():
+    with open(figer_gold_fp, 'r') as in_f, open(figer_gold_bio_fp, 'w+') as out_f:
+        for line in in_f:
+            if line != '\t\n' and line != '\n':
+                word, tags = line.rstrip().split('\t')
+
+                if tags != 'O':
+                    bio, tags = tags.split('-')
+                    tags = tags.replace('/', '', 1).replace(',/', ',').replace('/', '.').upper()
+                    tags = bio + '-' + tags
+
+                out_f.write(word + '\t' + tags + '\n')
+            else:
+                out_f.write('\n')
+
+
+def convert_figer_gold_to_not_bio():
+    with open(figer_gold_fp, 'r') as in_f, open(figer_gold_not_bio_fp, 'w+') as out_f:
+        for line in in_f:
+            if line != '\t\n' and line != '\n':
+                word, tags = line.rstrip().split('\t')
+
+                if tags != 'O':
+                    bio, tags = tags.split('-')
+                    tags = tags.replace('/', '', 1).replace(',/', ',').replace('/', '.').upper()
+
+                out_f.write(word + '\t' + tags + '\n')
+            else:
+                out_f.write('\n')
