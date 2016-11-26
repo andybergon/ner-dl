fc0cba8dbebb2c4e96d996dd93a78263ffc02386
andybergon <andybergon@gmail.com>
2016-11-26 19:07:05 +0100

    dataset splitter shortener w/ FIGER defaults

---
+++ nerdl/dataset/dataset_splitter_shortener.py
@@ -0,0 +1,44 @@
+import random
+
+random.seed(0)  # for debugging
+
+
+class DatasetSplitterShortener:
+    def __init__(self, dataset_fp, train_fp, test_fp):
+        self.dataset_fp = dataset_fp
+        self.train_fp = train_fp
+        self.test_fp = test_fp
+
+    def split_and_shorten(self, min_len=15, max_len=50, test_perc=0.2, sentence_nb=0, print_every=100000):
+        processed_sentences_nb = 0
+        tagged_words = []
+
+        with open(self.dataset_fp) as in_f, open(self.train_fp, 'w') as train_f, open(self.test_fp, 'w') as test_f:
+            for line in in_f:
+                if line != '\n':
+                    word, tag = line.rstrip().split('\t')
+                    tagged_words.append((word, tag))
+                else:
+                    sentence_len = len(tagged_words)
+                    if min_len <= sentence_len <= max_len:
+                        if random.uniform(0, 1) > test_perc:
+                            to_write_f = train_f
+                        else:
+                            to_write_f = test_f
+
+                        for word, tag in tagged_words:
+                            to_write_f.write('{}\t{}\n'.format(word, tag))
+                        to_write_f.write('\n')
+
+                        processed_sentences_nb += 1
+
+                        if processed_sentences_nb % print_every == 0:
+                            print('Writing Sentence #{}/{}.'.format(processed_sentences_nb, sentence_nb))
+
+                        if processed_sentences_nb == sentence_nb:
+                            print('Wrote #{}/{} sentences.'.format(processed_sentences_nb, sentence_nb))
+                            return
+
+                    tagged_words = []  # reset current sentence in any case
+
+        print('Wrote ALL sentences. (#{}/{})'.format(processed_sentences_nb, sentence_nb))
