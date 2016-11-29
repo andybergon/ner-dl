0d6372c7d588e5d0c770bb88c2813cab10a64d19
andybergon <andybergon@gmail.com>
2016-11-29 17:10:30 +0100

    minor style fix

--- nerdl/dataset/taggers/figer_tagger.py
+++ nerdl/dataset/taggers/figer_tagger.py
@@ -10,7 +10,7 @@ class FigerTagger(Tagger):
         self.initialize_map()
 
     def initialize_map(self):
-        with open(self.mapping_file, 'rt') as mapping_f:
+        with open(self.mapping_file, 'r') as mapping_f:
             for line in mapping_f:
                 freebase_type, figer_type = line.rstrip('\n').split('\t')
                 freebase_type = freebase_type.replace('/', '', 1).replace('/', '.')
@@ -24,8 +24,8 @@ class FigerTagger(Tagger):
     def convert_freebase_to_figer_types(self, freebase_types):
         """
 
-        :param freebase_types: e.g. [AWARD.AWARD, MUSIC.ALBUM]
-        :return: e.g. [AWARD, MUSIC]
+        :param freebase_types: e.g. [award.award, music.album]
+        :return: e.g. [award, music]
         """
         figer_types = []
 
