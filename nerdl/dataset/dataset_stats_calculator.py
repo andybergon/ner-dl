c99976b58c65973b3971e569d3bf73b737f6ecce
andybergon <andybergon@gmail.com>
2016-12-04 04:59:12 +0100

    type distribution in dataset stats

--- nerdl/dataset/dataset_stats_calculator.py
+++ nerdl/dataset/dataset_stats_calculator.py
@@ -5,6 +5,36 @@ class DatasetStatsCalculator:
     def __init__(self, dataset_fp):
         self.dataset_fp = dataset_fp
 
+    def calculate_type_distribution(self, sentence_nb=0, print_every=1000000):
+        type2occ = {}
+        current_sentence = 0
+
+        with open(self.dataset_fp) as ds_f:
+            for line in ds_f:
+                if line != '\n':
+                    word, tag = line.rstrip().split('\t')
+                    if tag != 'O':
+                        bi, tag = tag.split('-')
+                        types = tag.split(',')
+                        for type in types:
+                            if type in type2occ:
+                                type2occ[type] += 1
+                            else:
+                                type2occ[type] = 1
+                else:
+                    current_sentence += 1
+
+                    if print_every != 0 and current_sentence % print_every == 0:
+                        print_sentences_types_stats(type2occ)
+
+                    if current_sentence == sentence_nb:
+                        print_sentences_types_stats(type2occ)
+                        return
+
+            print_sentences_types_stats(type2occ)
+
+        return type2occ
+
     def calculate_sentence_length(self, sentence_nb=0, print_every=1000000):
         len2occ = {}
         current_sentence = 0
@@ -33,6 +63,24 @@ class DatasetStatsCalculator:
             print_sentences_length_stats(len2occ)
 
         return len2occ
+
+
+def print_sentences_types_stats(type2occ):
+    occ_sum = 0
+
+    type2occ['common.topic'] = 0  # reset, always present
+
+    for ix in type2occ:
+        occ_sum += type2occ[ix]
+
+    cum_perc = 0
+    top_types = sorted(type2occ.items(), key=lambda x: x[1], reverse=True)[:50]
+    for k, v in top_types:
+        perc = v / occ_sum
+        cum_perc += perc
+        print('{}\t{}\t{:.3%}\t{:.3%}'.format(k, v, perc, cum_perc))
+
+    print('Total Types Tags: {}\nUnique Tags: {}'.format(occ_sum, len(type2occ)))
 
 
 def print_sentences_length_stats(len2occ):
--- runners/run_dataset_stats.py
+++ runners/run_dataset_stats.py
@@ -2,7 +2,10 @@ from nerdl.dataset.dataset_stats_calculator import DatasetStatsCalculator
 from settings import path_settings
 
 # dataset_fp = path_settings.FIGER_DATASET_CW_TAGS_FILE
-dataset_fp = path_settings.CW_TRAIN_FILE
+dataset_fp = path_settings.CW_DATASET_CW_TAGS_FILE
+# dataset_fp = path_settings.CW_TRAIN_FILE
 # dataset_fp = path_settings.CW_TEST_FILE
 
-DatasetStatsCalculator(dataset_fp).calculate_sentence_length(sentence_nb=0, print_every=0)
+dsc = DatasetStatsCalculator(dataset_fp)
+# dsc.calculate_sentence_length(sentence_nb=0, print_every=0)
+dsc.calculate_type_distribution(sentence_nb=100000, print_every=0)
