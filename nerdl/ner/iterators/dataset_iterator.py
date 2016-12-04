fb473a61fc1b1d70a550f6444966bee7a1c6bfe5
andybergon <andybergon@gmail.com>
2016-12-04 21:23:31 +0100

    iterator (sentences, dataset, model) refactor

--- nerdl/ner/iterators/dataset_iterator.py
+++ nerdl/ner/iterators/dataset_iterator.py
@@ -3,26 +3,7 @@ from nerdl.ner.iterators.sentences_iterator import SentencesIterator
 
 class DatasetIterator(SentencesIterator):
     def __init__(self, file_fp):
-        self.file_fp = file_fp
-        self.is_file_open = None
-        self.file_f = self.open_file()
-
-    def open_file(self):
-        self.is_file_open = True
-        return open(self.file_fp, 'r')
-
-    def close_file(self):
-        print('EOF reached. Closing {} DatasetIterator file.'.format(self.file_fp))
-        self.is_file_open = False
-        self.file_f.close()
-
-    def count_sentences(self):
-        count = 0
-        with open(self.file_fp, 'r') as f:
-            for line in f:
-                if line == '\n' or line == '\t\n':
-                    count += 1
-        return count
+        SentencesIterator.__init__(self, file_fp)
 
     def next(self):
         word_tag = []
--- nerdl/ner/iterators/model_iterator.py
+++ nerdl/ner/iterators/model_iterator.py
@@ -2,29 +2,10 @@ from nerdl.ner.iterators.sentences_iterator import SentencesIterator
 
 
 class ModelIterator(SentencesIterator):
-    def __init__(self, model, file_fp):
+    def __init__(self, file_fp, model):
+        SentencesIterator.__init__(self, file_fp)
+
         self.model = model
-
-        self.file_fp = file_fp
-        self.is_file_open = None
-        self.file_f = self.open_file()
-
-    def open_file(self):
-        self.is_file_open = True
-        return open(self.file_fp, 'r')
-
-    def close_file(self):
-        print('EOF reached. Closing {} ModelIterator file.'.format(self.file_fp))
-        self.is_file_open = False
-        self.file_f.close()
-
-    def count_sentences(self):
-        count = 0
-        with open(self.file_fp, 'r') as f:
-            for line in f:
-                if line == '\n' or line == '\t\n':
-                    count += 1
-        return count
 
     def next(self):
         word_tag = []
