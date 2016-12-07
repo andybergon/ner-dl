f80f1023a7b3b9b064356c54160d2b0c3cdc39dd
andybergon <andybergon@gmail.com>
2016-12-07 23:38:57 +0100

    training runner cleaning

--- runners/run_train.py
+++ runners/run_train.py
@@ -11,39 +11,38 @@ from settings import settings
 start = time.time()
 
 ################################################################################
-# settings.MAX_SENTENCE_LEN = 80
-# k_sentences = 1000
-# path_settings.WORD2VEC_TXT_FILE = path_settings.CW_W2V_TXT_FILE.replace('.txt', '-old.txt')
-# path_settings.TRAINING_FILE = path_settings.TRAINING_FILE
-#                               .replace('training.tsv', 'training_{}k.tsv'.format(k_sentences))
-# path_settings.TEST_FILE = path_settings.TEST_FILE.replace('test.tsv', 'test_{}k.tsv'.format(k_sentences))
-################################################################################
 # settings.MAX_SENTENCE_LEN
 # settings.TAG2VEC_CLASS_LIST
+# path_settings.WORD2VEC_TXT_FILE
+# path_settings.TRAINING_FILE
+# path_settings.TEST_FILE
+# path_settings.TRAINING_CHECKPOINT_FILE
+# path_settings.MODEL_FILE
 
 settings.MAX_SENTENCE_LEN = 50
 
-settings.TAG2VEC_CLASS_LIST = settings.STANFORD_FOUR_CLASS_LIST
-# settings.TAG2VEC_CLASS_LIST = settings.FIGER_CLASS_LIST
+# settings.TAG2VEC_CLASS_LIST = settings.STANFORD_FOUR_CLASS_LIST
+settings.TAG2VEC_CLASS_LIST = settings.FIGER_CLASS_LIST
 # settings.TAG2VEC_CLASS_LIST = settings.CW_TOP275_CLASS_LIST
 
 # path_settings.WORD2VEC_TXT_FILE = path_settings.CW_W2V_TXT_FILE.replace('.txt', '-old.txt')
-path_settings.WORD2VEC_TXT_FILE = path_settings.CW_W2V_TXT_FILE
-path_settings.TRAINING_FILE = path_settings.CW_TRAIN_FILE
-path_settings.TEST_FILE = path_settings.CW_TEST_FILE
+# path_settings.WORD2VEC_TXT_FILE = path_settings.CW_W2V_TXT_FILE
+# path_settings.TRAINING_FILE = path_settings.CW_TRAIN_FILE
+# path_settings.TEST_FILE = path_settings.CW_TEST_FILE
 
-# path_settings.WORD2VEC_TXT_FILE = path_settings.FIGER_W2V_TXT_FILE
+path_settings.WORD2VEC_TXT_FILE = path_settings.FIGER_W2V_TXT_FILE
 # path_settings.TRAINING_FILE = path_settings.FIGER_TRAIN_FILE
-# path_settings.TEST_FILE = path_settings.FIGER_TEST_FILE
+path_settings.TRAINING_FILE = path_settings.FIGER_TRAIN_FILE.replace('.tsv', '-top100k-lines.tsv')
+path_settings.TEST_FILE = path_settings.FIGER_TEST_FILE
+
+path_settings.TRAINING_CHECKPOINT_FILE = path_settings.TRAINING_FILE.replace('.tsv', '-checkpoint.tsv')
 
 dt = datetime.datetime.now().isoformat()
 path_settings.MODEL_FILE = path_settings.MODEL_FILE.replace('.h5', '_{}.h5'.format(dt))
 
-path_settings.TRAINING_CHECKPOINT_FILE = path_settings.TRAINING_FILE.replace('.tsv', '-checkpoint.tsv')
-
 # tagger = None
-tagger = StanfordFourTagger()
-# tagger = FigerTagger()
+# tagger = StanfordFourTagger()
+tagger = FigerTagger()
 # tagger = CWTop275Tagger()
 
 model = KerasNERModel(tagger=tagger)
